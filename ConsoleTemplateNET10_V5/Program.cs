55310d90b4c45f9caed7387814001c57737484db
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2026-03-04 15:15:15 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleTemplateNET10_V5/Program.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2026
+// </copyright>
+// <Template>
+// 	Version 3.0.2026.1, 08.1.2026
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>03.03.2026 14:26:39</date>
+//
+// <summary>
+// Konsolen Applikation mit Menü
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace ConsoleTemplateNET10_V5
+{
+    /* Imports from NET Framework */
+    using System;
+
+    public class Program
+    {
+        public Program()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.CursorVisible = false;
+        }
+        private static void Main(string[] args)
+        {
+            CMenu unterMenu = new CMenu("Untermenü");
+            unterMenu.AddItem("Untermenüpunkt 1", () => UnterMenuPoint("A"), "🖥");
+            unterMenu.AddItem("Untermenüpunkt 2", () => UnterMenuPoint("B"), "🔊");
+
+            CMenu mainMenu = new CMenu("Hauptmenü");
+            mainMenu.AddItem("Auswahl Menüpunkt 1", MenuPoint1);
+            mainMenu.AddSubMenu("Einstellungen", unterMenu, "⚙");
+            mainMenu.AddItem("Beenden", () => ApplicationExit());
+            mainMenu.Show();
+        }
+
+        private static void ApplicationExit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            CMenu.Wait();
+        }
+
+        private static void UnterMenuPoint(string param)
+        {
+            Console.Clear();
+
+            CMenu.Wait(param);
+        }
+    }
+}
---
+++ ConsoleTemplateNET10_V5/README.md
@@ -0,0 +1,21 @@
+# Projektname
+
+![NET](https://img.shields.io/badge/NET-10.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2026](https://img.shields.io/badge/Visual%20Studio-2026-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2026.0-yellow.svg)]
+
+## Projekt für einen Window Konsolenprogramm
+Das kleine Konsolenprogramm dient zum Testen von Funktionen und Features in .NET 10.0 / C# 14. Es ist ein einfaches Template, das schnell angepasst werden kann, um verschiedene Funktionen oder Fingerübungen zu testen.
+
+## Beispielsource
+
+der Source ist soll auch einfache Art und Weise die Funktionen eines Features zeigen. Der Source ist so geschrieben, das so wenig wie möglich zusätzliche NuGet-Pakete benötigt werden.
+```csharp
+```
+
+```xml
+```
+
+```json
+```
